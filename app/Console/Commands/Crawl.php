d4060d0d6fd748d463ca90242351c3c445d13936
Fabio Ferreira <fabio.ferreira3@outlook.com>
2023-05-29 19:55:20 -0300

    crawl command

---
+++ app/Console/Commands/Crawl.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class Crawl extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'crawl {url}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $python = app()->environment('production') ? '/usr/bin/python' : '/usr/local/bin/python';
+
+        $script = app()->environment('production') ? '/app/crawler.py' : '/var/www/html/crawler.py';
+
+        $url = $this->argument('url');
+
+        $output = [];
+        exec("$python $script \"$url\"", $output);
+
+        return implode(PHP_EOL, $output);
+    }
+}
---
+++ crawler.py
@@ -0,0 +1,41 @@
+import requests
+from bs4 import BeautifulSoup
+import argparse
+
+# parser to handle command line arguments
+parser = argparse.ArgumentParser(description="Web scraping script")
+parser.add_argument("url", help="URL of the webpage to scrape")
+args = parser.parse_args()
+
+# headers to mimic a browser visit
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0;Win64) AppleWebkit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.82 Safari/537.36'
+}
+
+# make the HTTP GET request to the given url
+response = requests.get(args.url, headers=headers)
+response.raise_for_status()  # raise exception if invalid response
+
+# parse the HTML content
+soup = BeautifulSoup(response.content, 'html.parser')
+
+# find the 'title' tag
+title_tag = soup.find('title')
+title_text = "Title: "
+if title_tag is not None:
+    title_text += title_tag.get_text()
+
+# find the 'main' tag and then find 'h1', 'h2', and 'p' tags inside it
+main_tag = soup.find('body')
+content_text = ""
+if main_tag is not None:
+    tags = main_tag.find_all(['h1', 'h2', 'h3', 'h4', 'h5' 'p'])
+
+    # iterate over each tag and extract the text
+    for tag in tags:
+        content_text += "\n\n" + tag.get_text()
+
+# merge title and content
+full_text = title_text + "\n" + content_text
+
+print(full_text)
