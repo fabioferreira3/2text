7f8f380a7d714bba5fd8ab9ce54e4810678b2d06
Fabio Ferreira <fabio.ferreira3@outlook.com>
2023-06-03 19:14:36 -0300

    field actions mobile

--- resources/views/livewire/common/field-actions.blade.php
+++ resources/views/livewire/common/field-actions.blade.php
@@ -1,15 +1,26 @@
 <div class="md:hidden">
     <x-dropdown persistent>
-        @if($regenerateAction)<x-dropdown.item icon="refresh" wire:loading.attr="disabled" wire:click='regenerate' label="AI Regenerate" />@endif
-        @if($historyAction)<x-dropdown.item icon="user" separator label="View History" />@endif
-        @if($copyAction) <x-dropdown.item icon="clipboard-copy" wire:click='copy' separator :label='$copied ? "Copied" : "Copy"' :disabled='$copied ? true : false' />@endif
-        <x-dropdown.item icon="user" separator label="Save" />
+        @if($regenerateAction)
+            <x-dropdown.item icon="refresh">
+                <x-button sm spinner wire:loading.attr="disabled" wire:click='regenerate' label="AI Regenerate" class='hover:bg-transparent hover:shadow-none border-0 px-0 text-zinc-700' />
+            </x-dropdown.item>@endif
+        @if($historyAction)
+            <x-dropdown.item icon="book-open" separator>
+                <x-button sm wire:click='showHistoryModal' label="View History" class='border-0 px-0 text-zinc-700' />
+            </x-dropdown.item>@endif
+        @if($copyAction)
+        <x-dropdown.item icon="clipboard-copy" separator>
+            <x-button sm outline :disabled='$copied ? true : false' wire:click='copy' :label='$copied ? "Copied" : "Copy"' class='hover:bg-transparent hover:shadow-none border-0 px-0 text-zinc-700' />
+        </x-dropdown.item>@endif
+        <x-dropdown.item icon="save" separator>
+            <x-button sm spinner ire:loading.attr="disabled" wire:click='save' label='Save' class='hover:bg-transparent hover:shadow-none border-0 px-0 text-zinc-700' />
+        </x-dropdown.item>
     </x-dropdown>
 </div>
 <div class="hidden md:block">
     @if($regenerateAction)<x-button sm negative spinner wire:loading.attr="disabled" wire:click='regenerate' icon="refresh" label="AI Regenerate" class='rounded-lg' />@endif
     @if($historyAction)<x-button sm slate wire:click='showHistoryModal' icon="book-open" label="View History" class='rounded-lg' />@endif
     @if($copyAction) <x-button sm outline :disabled='$copied ? true : false' wire:click='copy' icon="clipboard-copy" :label='$copied ? "Copied" : "Copy"' class='border border-gray-300 rounded-lg' />@endif
-    <x-button sm dark spinner wire:click='save' icon="save" label='Save' class='font-bold rounded-lg' />
+    <x-button sm dark spinner ire:loading.attr="disabled" wire:click='save' icon="save" label='Save' class='font-bold rounded-lg' />
 </div>
 
