721bc427ada25bb458f4971fa0518cebfaa15b56
Fabio Ferreira <fabio.ferreira3@outlook.com>
2023-06-03 19:16:16 -0300

    field actions mobile

--- resources/views/livewire/common/field-actions.blade.php
+++ resources/views/livewire/common/field-actions.blade.php
@@ -3,15 +3,20 @@
         @if($regenerateAction)
             <x-dropdown.item icon="refresh">
                 <x-button sm spinner wire:loading.attr="disabled" wire:click='regenerate' label="AI Regenerate" class='hover:bg-transparent hover:shadow-none border-0 px-0 text-zinc-700' />
-            </x-dropdown.item>@endif
+            </x-dropdown.item>
+        @endif
+
         @if($historyAction)
             <x-dropdown.item icon="book-open" separator>
                 <x-button sm wire:click='showHistoryModal' label="View History" class='border-0 px-0 text-zinc-700' />
-            </x-dropdown.item>@endif
+            </x-dropdown.item>
+        @endif
+
         @if($copyAction)
         <x-dropdown.item icon="clipboard-copy" separator>
             <x-button sm outline :disabled='$copied ? true : false' wire:click='copy' :label='$copied ? "Copied" : "Copy"' class='hover:bg-transparent hover:shadow-none border-0 px-0 text-zinc-700' />
-        </x-dropdown.item>@endif
+        </x-dropdown.item>
+        @endif
         <x-dropdown.item icon="save" separator>
             <x-button sm spinner ire:loading.attr="disabled" wire:click='save' label='Save' class='hover:bg-transparent hover:shadow-none border-0 px-0 text-zinc-700' />
         </x-dropdown.item>
