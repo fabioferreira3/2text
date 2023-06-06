97e94ef987bbbeddb0cab7be7b77141b73b4b82e
Fabio Ferreira <fabioferreira@Fabios-MacBook-Pro.local>
2023-06-06 15:41:19 -0300

    dispatch all tasks job

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,7 +2,7 @@
 
 namespace App\Console;
 
-use App\Jobs\DispatchDocumentTasks;
+use App\Jobs\DispatchAllDocumentTasks;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
@@ -16,7 +16,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->job(new DispatchDocumentTasks())->everyMinute();
+        $schedule->job(new DispatchAllDocumentTasks())->everyMinute();
     }
 
     /**
