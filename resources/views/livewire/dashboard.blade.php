f947908f3d91fc21fef203b561af027a519251de
Fabio Ferreira <fabio.ferreira3@outlook.com>
2023-06-08 22:15:00 -0300

    minor layout fixes

--- resources/views/livewire/dashboard.blade.php
+++ resources/views/livewire/dashboard.blade.php
@@ -3,10 +3,10 @@
         @include('livewire.common.header', ['icon' => 'desktop-computer', 'label' => __('dashboard.dashboard')])
         <livewire:common.create-document />
     </div>
-    <div class="p-6 bg-white rounded-lg">
+    <div class="p-6 bg-zinc-100 rounded-lg">
         <div class='flex justify-between mb-6'>
             <h2 class="text-xl font-bold text-zinc-700">{{ __('dashboard.my_documents') }}</h2>
-            <x-button href='/dashboard/trash' icon='trash' white label="{{__('dashboard.trash')}}" />
+            <x-button href='/dashboard/trash' icon='trash' class="rounded-lg" white label="{{__('dashboard.trash')}}" />
         </div>
         <livewire:my-documents-table />
     </div>
--- resources/views/livewire/tables/my-documents/view-action.blade.php
+++ resources/views/livewire/tables/my-documents/view-action.blade.php
@@ -1,4 +1,4 @@
 <div class="flex items-center gap-2">
     <x-button.circle lg :disabled="!$isCompleted" white icon="arrow-circle-right" class="flex items-center gap-2" wire:click="viewDoc('{{$rowId}}')"/>
-    <x-button.circle sm :disabled="!$isCompleted" gray icon="trash" class="flex items-center gap-2" wire:click="deleteDoc('{{$rowId}}')"/>
+    <x-button.circle sm :disabled="!$isCompleted" gray icon="trash" class="flex items-center gap-2 bg-primary" wire:click="deleteDoc('{{$rowId}}')"/>
 </div>
