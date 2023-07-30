343fa4d5c051aa07c7a3e862ecfb93e3abf76a76
Fabio Ferreira <fabioferreira@Fabios-MacBook-Pro.local>
2023-07-30 00:30:43 -0300

    pusher vars

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,17 +1,6 @@
 import "./bootstrap";
 import Alpine from "alpinejs";
 import.meta.glob(["../images/**"]);
-import Echo from "laravel-echo";
-import Pusher from "pusher-js";
-
-window.Pusher = Pusher;
-
-window.Echo = new Echo({
-    broadcaster: "pusher",
-    key: import.meta.env.VITE_PUSHER_APP_KEY,
-    cluster: import.meta.env.VITE_PUSHER_APP_CLUSTER,
-    forceTLS: true,
-});
 
 window.Alpine = Alpine;
 
