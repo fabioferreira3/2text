848c527181692fd54f40eabbc52a144e55398a0f
Fabio Ferreira <fabio.ferreira3@outlook.com>
2023-08-09 21:15:03 -0300

    aborted document

--- app/Jobs/DispatchDocumentTasks.php
+++ app/Jobs/DispatchDocumentTasks.php
@@ -9,7 +9,6 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
-use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Bus;
 use Illuminate\Support\Facades\DB;
 
