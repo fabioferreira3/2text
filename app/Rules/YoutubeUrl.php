ba5d743a630d8e715d820c23db24fbb77e433daa
Fabio Ferreira <fabioferreira@Fabios-MacBook-Pro-2.local>
2023-12-05 22:19:01 -0300

    changes

--- app/Rules/YoutubeUrl.php
+++ app/Rules/YoutubeUrl.php
@@ -5,8 +5,9 @@ namespace App\Rules;
 use Closure;
 use Illuminate\Contracts\Validation\ValidationRule;
 
-class YoutubeUrl implements ValidationRule
+class YouTubeUrl implements ValidationRule
 {
+
     /**
      * Run the validation rule.
      *
@@ -14,6 +15,19 @@ class YoutubeUrl implements ValidationRule
      */
     public function validate(string $attribute, mixed $value, Closure $fail): void
     {
-        //
+        if (!(preg_match('#^https?://(?:www\.)?youtube\.com/watch\?v=[A-Za-z0-9_-]+(&.*)?$#', $value) ||
+            preg_match('#^https?://(?:www\.)?youtu\.be/[A-Za-z0-9_-]+(\?.*)?$#', $value))) {
+            $fail($this->message());
+        }
+    }
+
+    /**
+     * Get the validation error message.
+     *
+     * @return string
+     */
+    public function message()
+    {
+        return 'The provided URL is not a valid YouTube video URL.';
     }
 }
