c5b980185858c88a63382bc74c13a71ac28be1c3
Fabio Ferreira <fabio.ferreira3@outlook.com>
2024-01-06 18:59:29 -0300

    small changes

--- resources/views/livewire/common/navlink.blade.php
+++ resources/views/livewire/common/navlink.blade.php
@@ -1,4 +1,5 @@
-<a href="{{ route($route) }}" class="flex gap-4 items-center {{request()->routeIs($activeRoutes ?? $route) ? 'font-bold text-secondary' : 'text-white'}}">
+<a href="{{ route($route) }}"
+    class="flex gap-4 items-center {{request()->routeIs($activeRoutes ?? $route) ? 'font-bold text-secondary' : 'text-white hover:text-secondary transition duration-200'}}">
     <x-icon name={{$icon}} class="w-6 h-6" />
-    <span class="{{$submenu ?? false ? "xl:text-base" : "xl:text-xl"}}">{{$name}}</span>
+    <span class="{{$submenu ?? false ? " xl:text-base" : "xl:text-xl" }}">{{$name}}</span>
 </a>
--- resources/views/livewire/common/template.blade.php
+++ resources/views/livewire/common/template.blade.php
@@ -1,5 +1,6 @@
-<div wire:click='execute' class="flex flex-col gap-3 p-6 border bg-white rounded-lg hover:border-secondary hover:border-2 hover:cursor-pointer">
+<div wire:click='execute'
+    class="flex flex-col gap-3 p-6 border-2 border-gray-100 bg-white rounded-lg hover:border-secondary hover:cursor-pointer transition duration-500">
     <x-icon name={{$icon}} class="text-secondary w-16 h-16" />
-    <div class="font-bold text-xl text-primary">{{ $title }}</div>
-    <div>{{ $description }}</div>
+    <div class="font-bold text-xl text-gray-700">{{ $title }}</div>
+    <div class="text-gray-600">{{ $description }}</div>
 </div>
