6399256fde92a284c425544f06ba581920dbe732
Fabio Ferreira <fabio.ferreira3@outlook.com>
2024-02-26 19:50:56 -0300

    testimonials on register

---
+++ resources/views/components/testimonial.blade.php
@@ -0,0 +1,9 @@
+<div class="border border-gray-800 rounded-2xl p-4 px-6 bg-gray-900">
+    <div class="flex flex-col text-center h-full justify-center gap-4">
+        <p class="text-lg italic text-gray-300">"{{$quote}}"</p>
+        <p class="flex items-center justify-center gap-4 text-sm mt-2 text-gray-300">
+            <img class="rounded-full object-cover h-14 w-14" src="{{$img}}" alt="logo" />
+            {{$author}}
+        </p>
+    </div>
+</div>
