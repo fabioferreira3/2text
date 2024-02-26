e5fcbbaba13afd87aa8f6a9d101e7c538e7784e8
Fabio Ferreira <fabio.ferreira3@outlook.com>
2024-02-26 10:10:08 -0300

    gtag

--- resources/views/components/layouts/guest.blade.php
+++ resources/views/components/layouts/guest.blade.php
@@ -2,6 +2,18 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
 <head>
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=GTM-TNBCJZH9"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+
+            function gtag() {
+                dataLayer.push(arguments);
+            }
+            gtag('js', new Date());
+
+            gtag('config', 'GTM-TNBCJZH9');
+    </script>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <meta name="csrf-token" content="{{ csrf_token() }}" />
@@ -27,18 +39,6 @@
     <div class="font-sans text-gray-900 antialiased">
         {{ $slot }}
     </div>
-    <!-- Google tag (gtag.js) -->
-    <script async src="https://www.googletagmanager.com/gtag/js?id=GTM-TNBCJZH9"></script>
-    <script>
-        window.dataLayer = window.dataLayer || [];
-
-        function gtag() {
-            dataLayer.push(arguments);
-        }
-        gtag('js', new Date());
-
-        gtag('config', 'GTM-TNBCJZH9');
-    </script>
     @stack('scripts')
 </body>
 
