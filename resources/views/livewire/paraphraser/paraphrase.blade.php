af031a592068e11996c9ac7fafe3d8689bbd47eb
Fabio Ferreira <fabio.ferreira3@outlook.com>
2024-03-01 20:39:35 -0300

    remove paraphrased tone

--- resources/views/livewire/paraphraser/paraphrase.blade.php
+++ resources/views/livewire/paraphraser/paraphrase.blade.php
@@ -6,14 +6,19 @@
     'editable' => true,
     'document' => $document
     ])
+    <div class="m-auto flex mt-4 justify-start">
+        <div class="bg-gray-200 px-3 py-1 rounded-lg text-gray-700 text-lg md:text-sm font-semibold">
+            1 {{__('common.unit')}} = 480 {{__('common.words')}}
+        </div>
+    </div>
     {{-- @include('livewire.common.header', ['icon' => 'switch-horizontal', 'title' =>
     __('paraphraser.paraphrase_text')]) --}}
     @endsection
-    <div class="flex flex-col md:flex-row items-center justify-center gap-4 border-b py-4">
+    {{-- <div class="flex flex-col md:flex-row items-center justify-center gap-4 border-b py-4">
         <div>
             @include('livewire.paraphraser.tones', ['tone' => $tone])
         </div>
-    </div>
+    </div> --}}
     <div class="flex flex-col mt-4">
         <!-- Headers -->
         <div class="hidden md:grid md:grid-cols-2 gap-4 py-4">
@@ -44,8 +49,8 @@
                     @include('livewire.common.label', ['title' => __('paraphraser.original_text')])
                 </div>
                 <div>
-                    <textarea rows="10" class="w-full p-4 border border-zinc-200 rounded-lg"
-                        wire:model.live="inputText"></textarea>
+                    <textarea placeholder="Write here the text you want to be paraphrased..." rows="10"
+                        class="w-full p-4 border border-zinc-200 rounded-lg" wire:model.live="inputText"></textarea>
                     @if($errors->has('inputText'))
                     <span class="text-red-500 text-sm">{{ $errors->first('inputText') }}</span>
                     @endif
@@ -75,6 +80,10 @@
                     @livewire('common.blocks.text-block', [$contentBlock], key($contentBlock->id))
                     @endforeach
                 </div>
+            </div>
+            @else
+            <div class="mt-24 text-xl flex justify-center text-gray-600 w-full h-full italic">No text to be paraphrased.
+                Yet...
             </div>
             @endif
         </div>
