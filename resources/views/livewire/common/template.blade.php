445ae081d2eb29364770d290d7c119280047349b
Fabio Ferreira <fabio.ferreira3@outlook.com>
2024-03-14 09:47:03 -0300

    fix

--- resources/views/livewire/common/template.blade.php
+++ resources/views/livewire/common/template.blade.php
@@ -1,6 +1,8 @@
 <div wire:click='execute'
-    class="flex flex-col gap-3 p-6 border-2 border-gray-100 bg-white rounded-lg hover:border-secondary hover:cursor-pointer transition duration-500">
-    <x-icon name={{$icon}} class="text-secondary w-16 h-16" />
-    <div class="font-bold text-xl text-gray-700">{{ $title }}</div>
-    <div class="text-gray-600">{{ $description }}</div>
+    class="flex items-center xl:gap-0 md:gap-3 p-3 border-2 border-gray-100 bg-white rounded-lg hover:border-secondary hover:cursor-pointer transition duration-500">
+    <x-icon name={{$icon}} class="text-secondary w-1/4 h-16" />
+    <div class="flex flex-col w-3/4">
+        <div class="font-bold text-xl text-gray-700">{{ $title }}</div>
+        <div class="text-gray-600">{{ $description }}</div>
+    </div>
 </div>
